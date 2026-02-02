7c205150006391a0272b02e77b48cc9645557906
João Lucas <lgbr212@gmail.com>
2026-02-02 16:29:57 -0300

    Adicionando href ao menuOption e transformando em Link

--- src/components/menuOption/index.tsx
+++ src/components/menuOption/index.tsx
@@ -1,24 +1,35 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import styles from "./styles.module.scss";
 import { IconType } from "react-icons";
+import { useEffect, useState } from "react";
+import { useMenuOption } from "@/hooks/useMenuOption";
+import Link from "next/link";
 
 type MenuOptionsProps = {
   MenuIcon: IconType;
   menuTitle: string;
   isSelected: boolean;
+  href: string;
 };
 
-const MenuOption = ({ MenuIcon, menuTitle, isSelected }: MenuOptionsProps) => {
+const MenuOption = ({
+  MenuIcon,
+  menuTitle,
+  isSelected,
+  href,
+}: MenuOptionsProps) => {
   return (
-    <div
+    <Link
+      href={href}
       className={`${styles.menuOptionContainer} ${isSelected && styles.selected}`}
     >
       <MenuIcon
         className={`${styles.menuIcon} ${isSelected && styles.selected}`}
       />
       <h4 className={`${isSelected && styles.selected}`}>{menuTitle}</h4>
-    </div>
+    </Link>
   );
 };
 
