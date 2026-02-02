c7b925f3444ddaef07f5b550b2fbbeb1e27ed176
João Lucas <lgbr212@gmail.com>
2026-02-02 16:34:46 -0300

    Criando lógica de hook para pegar a rota atual

---
+++ src/hooks/useMenuOption.ts
@@ -0,0 +1,22 @@
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function useMenuOption() {
+  const pathname = usePathname();
+  const [actualPathname, setActualPathname] = useState<string>();
+
+  useEffect(() => {
+    function getActualPath() {
+      if (pathname.includes("dashboard")) {
+        setActualPathname("dashboard");
+      } else if (pathname.includes("producao")) {
+        setActualPathname("producao");
+      } else if (pathname.includes("metas")) {
+      }
+    }
+
+    getActualPath();
+  });
+
+  return [actualPathname];
+}
