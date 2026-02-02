df66bfe152b843fee927c1a6a55d3a359f4f9365
João Lucas <lgbr212@gmail.com>
2026-02-02 16:32:47 -0300

    Passando página principal para dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+import PageHeader from "@/components/pageHeader";
+import styles from "./page.module.scss";
+import { MdDashboard } from "react-icons/md";
+import { IoMdClipboard } from "react-icons/io";
+import ChartSection from "@/components/chartSection";
+import ProductionRegisterSection from "@/components/productionRegisterSection";
+import { LuGoal } from "react-icons/lu";
+import GoalSection from "@/components/goalSection";
+
+export default function Home() {
+  return (
+    <div className={styles.pageContainer}>
+      <PageHeader HeaderIcon={MdDashboard} headerTitle="Dashboard" />
+      <main className={styles.mainContainer}>
+        <h2>Análises</h2>
+        <ChartSection />
+        <h2>
+          <IoMdClipboard /> Registros de produção pendentes
+        </h2>
+        <ProductionRegisterSection />
+        <h2>
+          <LuGoal /> Metas
+        </h2>
+        <GoalSection />
+      </main>
+    </div>
+  );
+}
