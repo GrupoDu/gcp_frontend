d04c450791eabe7f519a886ff209fee3a9eaf1fb
jukalbf <lgbr212@gmail.com>
2026-03-11 17:32:17 -0300

    refactor(socket): Juntando urls em uma const só

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const socket = io(API_URL, {
+const DEV_API_URL = process.env.NEXT_PUBLIC_DEV_API_URL
+const URL = API_URL || DEV_API_URL;
+
+export const socket = io(URL, {
   transports: ["websocket", "polling"],
   withCredentials: true,
   timeout: 20000,
