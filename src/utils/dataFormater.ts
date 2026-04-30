3c07198a435655f536a9268ebc8f85d8ba48cb46
jukalbf <lgbr212@gmail.com>
2026-04-30 17:14:59 -0300

    fix: Correção de bug no dataFormater onde toISOString era chamado em string

--- src/utils/dataFormater.ts
+++ src/utils/dataFormater.ts
@@ -3,9 +3,11 @@ export function dataFormater(date: unknown): string {
   const isInvalidType = typeof date !== "string" && !(date instanceof Date);
   if (isInvalidType) throw new Error("Formato de data inválido.");
 
-  if (typeof date === "string") dataPart = date.split("T")[0];
-
-  dataPart = (date as Date).toISOString().split("T")[0];
+  if (typeof date === "string") {
+    dataPart = date.split("T")[0];
+  } else {
+    dataPart = (date as Date).toISOString().split("T")[0];
+  }
 
   const [ano, mes, dia] = dataPart.split("-");
   return `${dia}/${mes}/${ano}`;
