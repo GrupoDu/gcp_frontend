9710a259303960a129d8a3aba8c244272e63e7ad
jukalbf <lgbr212@gmail.com>
2026-06-12 15:10:24 -0300

    feat: Update employee and user management pages and forms

--- src/app/(pages)/usuarios/register/page.tsx
+++ src/app/(pages)/usuarios/register/page.tsx
@@ -4,12 +4,14 @@ import styles from "./page.module.scss";
 import { FaUserCog } from "react-icons/fa";
 import UserForm from "@/components/forms/userForm";
 import { UserProvider } from "@/providers/users.provider";
+import { Breadcrumb } from "@/components/breadcrumb";
 
 const RegisterUserPage = () => {
   return (
     <div className="pageContainer">
       <PageHeader headerTitle="Usuários" HeaderIcon={FaUserCog} />
       <main className="mainContainer">
+        <Breadcrumb />
         <h3>Registrar novo usuário</h3>
         <UserProvider>
           <UserForm />
