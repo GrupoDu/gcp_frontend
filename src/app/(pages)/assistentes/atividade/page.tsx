f2b4afdfe07cc34ac0985b8a2fd360f624fa51b1
jukalbf <lgbr212@gmail.com>
2026-07-03 08:22:44 -0300

    feat(assisAct): Ajustando layout

--- src/app/(pages)/assistentes/atividade/page.tsx
+++ src/app/(pages)/assistentes/atividade/page.tsx
@@ -1,15 +1,17 @@
 import PageHeader from "@/components/ui/pageHeader";
 import { IoMdClipboard } from "react-icons/io";
 import { AssistantActivityForm } from "@/components/forms/assistantActivityForm";
+import { Breadcrumb } from "@/components/breadcrumb";
 
 function AssistantActivityPage() {
   return (
     <div className={"pageContainer"}>
       <PageHeader HeaderIcon={IoMdClipboard} headerTitle={"Atividades de Assistentes"} />
       <main className={"mainContainer"}>
+        <Breadcrumb />
         <h2>Registrar Atividade</h2>
+        <AssistantActivityForm />
       </main>
-      <AssistantActivityForm />
     </div>
   );
 }
