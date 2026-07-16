67544ee2631fb36dfa952fe27e69fa71bf3fe28c
jukalbf <lgbr212@gmail.com>
2026-07-16 16:20:02 -0300

    Iniciando refatoração para apiV2

--- src/components/forms/activityForm/index.tsx
+++ src/components/forms/activityForm/index.tsx
@@ -11,6 +11,8 @@ import { useFetch } from "@/hooks/useFetch";
 import { DateInput } from "@/components/ui/dateInput";
 import { Product } from "@/types/product.interface";
 import { DefaultButton } from "@/components/ui/defaultButton";
+import { Product } from "@/types/product.interface";
+import { Employee } from "@/types/employee.interface";
 
 const ActivityForm = () => {
   const [deadline, setDeadline] = useState("");
