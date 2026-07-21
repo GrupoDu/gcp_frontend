e3ce8f6dfefe4bfc04f31e9c78e759ce19d95036
jukalbf <lgbr212@gmail.com>
2026-07-21 11:28:02 -0300

    refactor(prodOrd): lista de filtro e display
    - Adicionando return na função de display(duh)
    - Removendo const de list filtrada

--- src/components/cardLists/productionOrderList.tsx
+++ src/components/cardLists/productionOrderList.tsx
@@ -7,6 +7,7 @@ import { useSearchParams } from "next/navigation";
 import { ProductionOrder } from "@/types/productionOrder.type";
 import { useFetch } from "@/hooks/useFetch";
 import DataNotFound from "@/components/dataNotFound";
+import { useEffect } from "react";
 
 const ProductionOrderList = () => {
   const { data: productionOrders, refetch } = useFetch<ProductionOrder[]>("production-orders");
@@ -15,18 +16,15 @@ const ProductionOrderList = () => {
   const statusFilter = searchParams.get("status");
   const deadlineFilter = searchParams.get("deadline");
   const employeeFilter = searchParams.get("employee");
-  const filteredList = productionOrders?.filter(
-    (order) =>
-      order.products.name === productFilter &&
-      order.production_order_status === statusFilter &&
-      order.production_order_deadline.toISOString() === deadlineFilter &&
-      order.welders?.employee_uuid === employeeFilter,
-  );
-  const isListEmpty = !filteredList || filteredList.length < 1;
+  const isListEmpty = !productionOrders || productionOrders.length < 1;
+
+  useEffect(() => {
+    console.log(productionOrders);
+  }, [productionOrders]);
 
   return (
     <ul className={`${styles.cardListContainer} ${isListEmpty && styles.emptyList}`}>
-      {displayProductionOrders(refetch, filteredList)}
+      {displayProductionOrders(refetch, productionOrders)}
     </ul>
   );
 };
@@ -34,7 +32,7 @@ const ProductionOrderList = () => {
 function displayProductionOrders(refetch: () => void, orders?: ProductionOrder[]) {
   if (!orders || orders.length < 1) return <DataNotFound />;
 
-  orders?.map((order) => (
+  return orders?.map((order) => (
     <li key={order.production_order_uuid}>
       <CardProductionOrder
         date={dataFormater(order.production_order_deadline)}
