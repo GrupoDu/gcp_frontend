ff00795baf629503fdcb51827b317171b1a819bd
jukalbf <lgbr212@gmail.com>
2026-07-21 11:37:15 -0300

    refactor: Removendo imports e código desnecessário
    - Removendo implementação antiga de atividade
    - Removendo useState antigo não usado
    - Adicionando useState para atualização

--- src/components/forms/activityForm/index.tsx
+++ src/components/forms/activityForm/index.tsx
@@ -1,95 +1,39 @@
 "use client";
 
-import styles from "./styles.module.scss";
 import FormTemplate from "@/components/forms/formTemplate";
 import { toast } from "react-toastify";
 import TextInput from "@/components/ui/textInput";
 import React, { useState } from "react";
-import SelectInput, { SelectOption } from "@/components/ui/selectInput";
-import { useRouter, useSearchParams } from "next/navigation";
-import LinkButton from "@/components/linkButton";
-import SubmitButton from "@/components/ui/submitButton";
+import SelectInput from "@/components/ui/selectInput";
+import { useRouter } from "next/navigation";
 import { api } from "@/services/api";
 import { useFetch } from "@/hooks/useFetch";
 import { Product } from "@/types/product.type";
-import { Employee } from "@/types/employee.type";
+import { DateInput } from "@/components/ui/dateInput";
+import { DefaultButton } from "@/components/ui/defaultButton";
 
 const ActivityForm = () => {
-  const [employee, setEmployee] = useState("");
-  const [producedQuantity, setProducedQuantity] = useState(0);
+  const [deadline, setDeadline] = useState("");
+  const [quantityToProduce, setQuantityToProduce] = useState(0);
   const [product, setProduct] = useState("");
-  const [activityType, setActivityType] = useState("");
-  const [activityDescription, setActivityDescription] = useState("");
   const { data: products } = useFetch<Product[]>("products");
-  const { data: employees } = useFetch<Employee[]>("employees");
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const employeeRole = searchParams.get("employee");
   const productOptions = products?.map((product) => ({
     value: product.product_uuid!,
     label: product.name,
   }));
 
-  const isWelder = employeeRole === "soldador";
-  const isAssistant = employeeRole === "assistente";
-  const assistantRoleOptions = [
-    {
-      value: "Corte",
-      label: "Corte",
-    },
-    {
-      value: "Pintura",
-      label: "Pintura",
-    },
-    {
-      value: "Acabamento",
-      label: "Acabamento",
-    },
-    {
-      value: "Dobra",
-      label: "Dobra",
-    },
-  ];
-
-  const handleWelderSubmit = async (e: React.SubmitEvent) => {
-    e.preventDefault();
-
-    try {
-      await api.post("/welders-activities/register", {
-        produced_quantity: producedQuantity,
-        product_uuid: product,
-        welder_uuid: employee,
-      });
-
-      router.push("/producao");
-    } catch (err) {
-      const error = err as Error;
-      toast.error(error.message);
-    }
-  };
-  const handleAssistantSubmit = async (e: React.SubmitEvent) => {
-    e.preventDefault();
-
-    try {
-      await api.post("assistants-activities/", {
-        produced_quantity: producedQuantity,
-        activity_type: activityType,
-        assistant_uuid: employee,
-        activity_description: activityDescription || "",
-      });
-
-      router.push("/producao");
-    } catch (err) {
-      const error = err as Error;
-      toast.error(error.message);
-    }
-  };
   const handleSubmit = async (e: React.SubmitEvent) => {
     e.preventDefault();
 
     try {
-      if (isWelder) return handleWelderSubmit(e);
-      if (isAssistant) return handleAssistantSubmit(e);
+      const payload = {
+        product_uuid: product,
+        quantity_to_produce: quantityToProduce,
+        production_order_deadline: new Date(deadline),
+      };
+
+      await api.post("/production-orders", payload);
 
       router.push("/producao");
       toast.success("Quantidade produzida registrada com sucesso");
@@ -98,80 +42,28 @@ const ActivityForm = () => {
       toast.error(error.message);
     }
   };
-  const lastInput = () => {
-    if (isWelder)
-      return (
-        <SelectInput
-          options={productOptions}
-          onChange={(e) => setProduct(e.target.value)}
-          defaultValue={"Selecione um produto"}
-          value={product}
-          label={"Produto produzido"}
-          required={true}
-        />
-      );
-
-    if (isAssistant)
-      return (
-        <label className={styles.activityDescriptionContainer}>
-          <h4>Descrição da atividade</h4>
-          <textarea value={activityDescription} onChange={(e) => setActivityDescription(e.target.value)} />
-        </label>
-      );
-  };
 
   return (
     <FormTemplate submitHandler={(e) => handleSubmit(e)}>
+      <DateInput label={"Prazo"} setValue={setDeadline} value={deadline} isFilter={false} />
       <SelectInput
-        options={selectFormatter(employees, employeeRole)}
-        defaultValue="Selecione o funcionário"
-        value={employee}
-        onChange={(e) => setEmployee(e.target.value)}
-        label="Soldador"
+        options={productOptions}
+        onChange={(e) => setProduct(e.target.value)}
+        defaultValue={"Selecione um produto"}
+        value={product}
+        label={"Produto a ser produzido"}
         required={true}
       />
-      {isAssistant && (
-        <SelectInput
-          options={assistantRoleOptions}
-          onChange={(e) => setActivityType(e.target.value)}
-          defaultValue={"Selecionar atividade"}
-          value={activityType}
-          label={"Atividade do assistente"}
-        />
-      )}
       <TextInput
-        label="Quantidade produzida"
-        type="number"
+        type={"number"}
+        onChange={(e) => setQuantityToProduce(parseInt(e.target.value))}
+        value={quantityToProduce}
         min={0}
-        value={producedQuantity}
-        onChange={(e) => setProducedQuantity(Number(e.target.value))}
-        required={true}
+        label={"Quantidade a ser produzida"}
       />
-      {lastInput()}
-      <div className={styles.buttonsContainer}>
-        <LinkButton href={"/producao"} color={"black"}>
-          Cancelar
-        </LinkButton>
-        <SubmitButton canEdit={false}>Registrar</SubmitButton>
-      </div>
+      <DefaultButton type={"submit"}>Registrar</DefaultButton>
     </FormTemplate>
   );
 };
-
-function selectFormatter(list: Employee[] | undefined, employeeRole: string | null): SelectOption[] | undefined {
-  return list?.map((employee) => {
-    if (employee.employee_role === employeeRole) {
-      return {
-        value: employee.employee_uuid!,
-        label: employee.name,
-      };
-    }
-
-    return {
-      value: "",
-      label: "",
-    };
-  });
-}
 
 export default ActivityForm;
