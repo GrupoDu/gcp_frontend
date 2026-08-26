6aaeae722770ae79867e2c38f15fc8634eef4d67
jukalbf <lgbr212@gmail.com>
2026-08-26 13:34:17 -0300

    feat: Aplicando filtros

--- src/components/productionContainer/index.tsx
+++ src/components/productionContainer/index.tsx
@@ -4,29 +4,43 @@ import styles from "./styles.module.scss";
 import Loading from "@/components/ui/loading";
 import OpenMobileProvider from "@/providers/openMobile.provider";
 import ListFooter from "@/components/listFooter";
-import FilterMobileContainer from "@/components/filterMobileContainer";
 import StatusDropdown from "@/components/ui/statusDropdown";
 import ProductsDropdown from "@/components/ui/productsDropdown";
-import DeadlineInput from "@/components/ui/deadlineInput";
 import FiltersList from "@/components/filtersList";
 import { useLoading } from "@/hooks/useLoading";
 import DataNotFound from "@/components/dataNotFound";
 import { useFetch } from "@/hooks/useFetch";
 import { ProductionOrder } from "@/types/productionOrder.interface";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import CardProductionOrder from "@/components/ui/cardProductionOrder";
 import { dataFormater } from "@/utils/dataFormater";
 import { titleFormatter } from "@/utils/titleFormatter";
+import SelectInput from "@/components/ui/selectInput";
+import { MONTHS_OPTIONS } from "@/constants/monthsOptions.constant";
+import { TRACK_PARAMS } from "@/constants/trackParams.constant";
+import { useState } from "react";
+import { setQueryParams } from "@/utils/setQueryParams";
 
 const ProductionContainer = () => {
   const { isLoading } = useLoading();
-  const { data: productionOrders, refetch } = useFetch<ProductionOrder[]>("productionOrder");
+  const [monthFilter, setMonthFilter] = useState("");
+  const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
-  const productFilter = searchParams.get("product");
-  const statusFilter = searchParams.get("status");
-  const deadlineFilter = searchParams.get("deadline");
-  const employeeFilter = searchParams.get("employee");
+  const hasFilters = searchParams.size > 0;
+  const endpoint = `productionOrder${hasFilters ? "/filter" : ""}`;
+  const { data: productionOrders, refetch } = useFetch<ProductionOrder[]>(endpoint, TRACK_PARAMS);
   const isListEmpty = !productionOrders || productionOrders.length < 1;
+
+  const handleMonthChange = (value: string) => {
+    setMonthFilter(value);
+    const params = setQueryParams({
+      searchParams,
+      value,
+      key: "month",
+    });
+    router.push(`${pathname}?${params}`);
+  };
 
   return (
     <>
@@ -38,15 +52,16 @@ const ProductionContainer = () => {
             hrefButton={"producao/atividade"}
             style={{ borderRadius: ".2rem .2rem 0 0", borderBottom: 0 }}
           >
-            <DeadlineInput />
+            <SelectInput
+              label={"Mês"}
+              options={MONTHS_OPTIONS}
+              defaultValue={"Filtrar por Mês"}
+              onChange={(e) => handleMonthChange(e.target.value)}
+              value={monthFilter}
+            />
             <ProductsDropdown />
             <StatusDropdown />
           </FiltersList>
-          <FilterMobileContainer>
-            <DeadlineInput />
-            <ProductsDropdown />
-            <StatusDropdown />
-          </FilterMobileContainer>
           <ul className={`${styles.cardListContainer} ${isListEmpty && styles.emptyList}`}>
             {isListEmpty && <DataNotFound />}
             {productionOrders &&
