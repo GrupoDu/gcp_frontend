9fe41b707b76ac630d511cf765e3e18e5cbe08ff
jukalbf <lgbr212@gmail.com>
2026-08-28 10:05:57 -0300

    ref(produção): Separando componentes
    Separando partes do componente que usam useFetch

---
+++ src/components/monthInputSelect/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import SelectInput from "../ui/selectInput";
+import { MONTHS_OPTIONS } from "@/constants/monthsOptions.constant";
+import { useState } from "react";
+import { setQueryParams } from "@/utils/setQueryParams";
+import { router } from "next/client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export const MonthInputSelect = () => {
+  const [monthFilter, setMonthFilter] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleMonthChange = (value: string) => {
+    setMonthFilter(value);
+    const params = setQueryParams({
+      searchParams,
+      value,
+      key: "month",
+    });
+    router.push(`${pathname}?${params}`);
+  };
+
+  return (
+    <>
+      <SelectInput
+        label={"Mês"}
+        options={MONTHS_OPTIONS}
+        defaultValue={"Filtrar por Mês"}
+        onChange={(e) => handleMonthChange(e.target.value)}
+        value={monthFilter}
+      />
+    </>
+  );
+};
--- src/components/productionContainer/index.tsx
+++ src/components/productionContainer/index.tsx
@@ -1,87 +1,43 @@
 "use client";
 
 import styles from "./styles.module.scss";
-import Loading from "@/components/ui/loading";
-import OpenMobileProvider from "@/providers/openMobile.provider";
 import ListFooter from "@/components/listFooter";
-import StatusDropdown from "@/components/ui/statusDropdown";
-import ProductsDropdown from "@/components/ui/productsDropdown";
-import FiltersList from "@/components/filtersList";
-import { useLoading } from "@/hooks/useLoading";
 import DataNotFound from "@/components/dataNotFound";
 import { useFetch } from "@/hooks/useFetch";
 import { ProductionOrder } from "@/types/productionOrder.interface";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import CardProductionOrder from "@/components/ui/cardProductionOrder";
 import { dataFormater } from "@/utils/dataFormater";
 import { titleFormatter } from "@/utils/titleFormatter";
-import SelectInput from "@/components/ui/selectInput";
-import { MONTHS_OPTIONS } from "@/constants/monthsOptions.constant";
 import { TRACK_PARAMS } from "@/constants/trackParams.constant";
-import { useState } from "react";
-import { setQueryParams } from "@/utils/setQueryParams";
 
 const ProductionContainer = () => {
-  const { isLoading } = useLoading();
-  const [monthFilter, setMonthFilter] = useState("");
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
   const hasFilters = searchParams.size > 0;
   const endpoint = `productionOrder${hasFilters ? "/filter" : ""}`;
   const { data: productionOrders, refetch } = useFetch<ProductionOrder[]>(endpoint, TRACK_PARAMS);
   const isListEmpty = !productionOrders || productionOrders.length < 1;
 
-  const handleMonthChange = (value: string) => {
-    setMonthFilter(value);
-    const params = setQueryParams({
-      searchParams,
-      value,
-      key: "month",
-    });
-    router.push(`${pathname}?${params}`);
-  };
-
   return (
     <>
-      {isLoading && <Loading />}
-      <OpenMobileProvider>
-        <main style={{ gap: 0 }} className={`${styles.listContainer} mainContainer ${isLoading ? "loading" : ""}`}>
-          <FiltersList
-            buttonLabel={"Ordem de produção"}
-            hrefButton={"producao/atividade"}
-            style={{ borderRadius: ".2rem .2rem 0 0", borderBottom: 0 }}
-          >
-            <SelectInput
-              label={"Mês"}
-              options={MONTHS_OPTIONS}
-              defaultValue={"Filtrar por Mês"}
-              onChange={(e) => handleMonthChange(e.target.value)}
-              value={monthFilter}
-            />
-            <ProductsDropdown />
-            <StatusDropdown />
-          </FiltersList>
-          <ul className={`${styles.cardListContainer} ${isListEmpty && styles.emptyList}`}>
-            {isListEmpty && <DataNotFound />}
-            {productionOrders &&
-              productionOrders.map((order) => (
-                <li key={order.productionOrderUuid}>
-                  <CardProductionOrder
-                    date={dataFormater(order.productionOrderDeadline)}
-                    deliveryDate={order.deliveredAt}
-                    description={order.productionOrderDescription || ""}
-                    title={titleFormatter(order.product.acronym, order.toBeProduced)}
-                    status={order.productionOrderStatus}
-                    registerId={order?.productionOrderUuid || ""}
-                    refetch={refetch}
-                  />
-                </li>
-              ))}
-          </ul>
-          <ListFooter status={["Em Produção", "Entregue", "Atrasado"]} />
-        </main>
-      </OpenMobileProvider>
+      <ul className={`${styles.cardListContainer} ${isListEmpty && styles.emptyList}`}>
+        {isListEmpty && <DataNotFound />}
+        {productionOrders &&
+          productionOrders.map((order) => (
+            <li key={order.productionOrderUuid}>
+              <CardProductionOrder
+                date={dataFormater(order.productionOrderDeadline)}
+                deliveryDate={order.deliveredAt}
+                description={order.productionOrderDescription || ""}
+                title={titleFormatter(order.product.acronym, order.toBeProduced)}
+                status={order.productionOrderStatus}
+                registerId={order?.productionOrderUuid || ""}
+                refetch={refetch}
+              />
+            </li>
+          ))}
+      </ul>
+      <ListFooter status={["Em Produção", "Entregue", "Atrasado"]} />
     </>
   );
 };
