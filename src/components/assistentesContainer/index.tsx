9f74252c3d79d55f48361db8da349868005b482e
jukalbf <lgbr212@gmail.com>
2026-09-02 13:35:25 -0300

    fix: Ajustando paginação de assistentes
    - Alterando declaração da função para const
    - Declarando displayList como função dentro de AssistantContainer

--- src/components/assistentesContainer/index.tsx
+++ src/components/assistentesContainer/index.tsx
@@ -8,7 +8,10 @@ import { Pagination } from "@/components/pagination";
 import { TableList } from "@/components/lists/tableList";
 import { hasFilters } from "@/utils/hasFilters";
 
-function AssistantContainer() {
+/**
+ * Componente container para exibição e controle da listagem de atividades dos assistentes.
+ */
+const AssistantContainer = () => {
   const searchParams = useSearchParams();
   const endpoint = `assistantActivity${hasFilters(searchParams) ? "/filter" : "/offset"}?${searchParams.toString()}`;
   const { data: activities, maxPages } = useFetch<AssistantsActivities[]>(endpoint);
@@ -16,27 +19,24 @@ function AssistantContainer() {
   const headValues = ["Assistente", "Atividade", "Qtd.", "Data"];
   const isListPopulated = !!activities && activities?.length > 0;
 
-  return (
-    <>
-      <TableList tHeadValues={headValues} isListPopulated={isListPopulated}>
-        <DisplayList activities={activities} />
-      </TableList>
-      <Pagination maxPage={maxPages} />
-    </>
-  );
-}
-
-function DisplayList({ activities }: { activities?: AssistantsActivities[] }) {
-  if (!activities || activities.length === 0) return [];
-
-  return activities?.map((activity) => (
-    <tr key={activity.assistantsActivitiesUuid}>
+  const displayList = activities?.map((activity, index) => (
+    <tr key={activity.assistantsActivitiesUuid || activity.assistantActivityUuid || index}>
       <td>{activity.employee.name}</td>
       <td>{activity.activityType}</td>
       <td>{activity.producedQuantity}</td>
       <td>{dataFormater(activity.registeredAt)}</td>
     </tr>
   ));
-}
+
+  return (
+    <>
+      <TableList tHeadValues={headValues} isListPopulated={isListPopulated}>
+        {displayList}
+      </TableList>
+      <Pagination maxPage={maxPages} />
+    </>
+  );
+};
 
 export default AssistantContainer;
+
