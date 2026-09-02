0e77a839af104c7f1a45ff35cde1a02957a82cd9
jukalbf <lgbr212@gmail.com>
2026-09-02 13:44:55 -0300

    ref(hasFilter): Validação por array
    Validando se existem filtros transformando searchParams em array

--- src/utils/hasFilters.ts
+++ src/utils/hasFilters.ts
@@ -1,1 +1,11 @@
-export const hasFilters = (searchParams: URLSearchParams) => searchParams.size > 2;
+import { isNotPageParams } from "@/utils/isNotPageParams";
+
+/**
+ * Verifica se os parâmetros de busca contêm filtros além dos parâmetros de paginação.
+ *
+ * @param searchParams - Instância de URLSearchParams
+ * @returns boolean indicando se há filtros ativos
+ */
+export const hasFilters = (searchParams: URLSearchParams) => {
+  return Array.from(searchParams.keys()).some((key) => isNotPageParams(key));
+};
